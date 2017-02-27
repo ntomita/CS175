d25716d2feda5d6c04639bea404678bb0a4ae7a5
TomitaNaofumi <Naofumi.Tomita.GR@dartmouth.edu>
2017-02-27 12:47:32 -0500

    fixed errors in mutation function

---
+++ mutation.py
@@ -0,0 +1,60 @@
+import operator
+
+def probable_mutations(original, pmf, location_param_dict, n, method='min'):
+    """Based on hsp sequences and Location parameters values of Amino acids,
+    calculate the probable mutations depending on a threshold value
+
+    Args:
+        original - the original sequence
+        pmf - the PMF distribution
+        n -  number of mutations given by the user
+        method (string): optimization method:
+            either minimization or maximization of objective function
+            acceptable input: 'min', 'max'
+
+    Returns:
+        (string, score): the mutated string and corresponding score
+    """
+
+    delta_tuples = []
+    # Step 1: traverse through the original string to carry out the mutations
+    for itr in xrange(0, len(original)):
+        #Step 2: for every character of the original string, look at the corresponding
+        #dictionary in the PMF
+        pmf_dict = pmf.get_distribution(itr)
+        str_char = original[itr]
+        if method == 'min':
+            curr_tuple = (None, float('Inf'), None)
+        else:
+            curr_tuple = (None, -float('Inf'), None)
+        # Step 3: traverse through all key value pairs in the pmf dictionary
+        for key in pmf_dict:
+            # Step 4: caclulate Delta of the scores of the key and string's character
+            delta = location_param_dict[key]-location_param_dict[str_char] # TODO: need null handle
+            if method == 'min' and delta < 0 and curr_tuple[1] > delta:
+                curr_tuple = (key, delta, itr)
+            elif method == 'max' and delta > 0 and curr_tuple[1] < delta:
+                curr_tuple = (key, delta, itr)
+
+        # Step 5: add the tuples into the corresponding index of the list of
+        # deltas for the original string
+        delta_tuples.append(curr_tuple)
+    if method == 'min':
+        sorted_delta_tuples = sorted(delta_tuples, key=lambda x: x[1], reverse=False)
+    else:
+        sorted_delta_tuples = sorted(delta_tuples, key=lambda x: x[1], reverse=True)
+
+    """Mutate the original string by taking highest n values of
+     deltas from sorted list"""
+    list_orig = list(original)
+
+    for i in xrange(n):
+        curr_t = sorted_delta_tuples[i]
+        list_orig[curr_t[2]] = curr_t[0]
+
+    mutated_string ="".join(list_orig)
+
+    #print original
+    #print mutated_string
+    score = 0 # calculate_score(mutated_string, location_param_dict)
+    return (mutated_string, score)
--- project.py
+++ project.py
@@ -1,57 +1,11 @@
 from blast import Blast
 import os
-import operator
-def probable_mutations(original,pmf,n):
-    """Based on hsp sequences and Location parameters values of Amino acids,
-    calculate the probable mutations depending on a threshold value
+from mutation import probable_mutations
 
-    Args:
-        original - the original sequence
-        pmf - the PMF distribution
-        n -  number of deltas given by the user
-
-        Returns:
-        mut_string - the mutated string
-    """
-
-    delta_tuples = []     
-    location_param_dict = {'A':-0.605,'C':-0.693,'D':-0.279,'E':-0.160,'F':-0.719
-                        ,'G':-0.537,'H':-.662,'I':-0.682,'K':-0.043,'L':-0.631
-                        ,'M':-0.626,'N':-0.381,'P':-0.271,'Q':-0.369,'R':-0.448
-                        ,'S':-0.423,'T':-0.525,'V':-0.669,'W':-0.727,'Y':-0.721} 
-    # Step 1: traverse through the original string to carry out the mutations
-    for itr in xrange(0, len(original)):
-        #Step 2: for every character of the original string, look at the corresponding
-        #dictionary in the PMF
-        pmf_dict = pmf.get_distribution(itr)
-        str_char = original[itr]
-        curr_tuple = (0,0,0)
-        # Step 3: traverse through all key value pairs in the pmf dictionary
-        for key in pmf_dict:
-            # Step 4: caclulate Delta of the scores of the key and string's character
-            delta = abs(location_param_dict[key]-location_param_dict[str_char])
-            if curr_tuple[1] <= delta:
-                curr_tuple = (key, delta, itr)
-
-        # Step 5: add the tuples into the corresponding index of the list of 
-        # deltas for the original string
-        delta_tuples.append(curr_tuple)
-
-
-    sorted_delta_tuples = sorted(delta_tuples, key=lambda x: x[1], reverse=True)
-    
-    """Mutate the original string by taking highest n values of
-     deltas from sorted list"""
-    list_orig = list(original)
-    
-    for i in xrange(n):
-        curr_t = sorted_delta_tuples[i]
-        list_orig[curr_t[2]] = curr_t[0]
-
-    mutated_string ="".join(list_orig)
-
-    #print original
-    #print mutated_string
+flexibility_dict = {'A':-0.605,'C':-0.693,'D':-0.279,'E':-0.160,'F':-0.719
+                      ,'G':-0.537,'H':-.662,'I':-0.682,'K':-0.043,'L':-0.631
+                      ,'M':-0.626,'N':-0.381,'P':-0.271,'Q':-0.369,'R':-0.448
+                      ,'S':-0.423,'T':-0.525,'V':-0.669,'W':-0.727,'Y':-0.721}
 
 def test():
     """Testing methods
@@ -79,9 +33,9 @@ def test():
     for i in range(10):
         print "at {}: {}".format(i, pmf.get_distribution(i))  # showing first 10 distributions
 
-    probable_mutations(original_string,pmf,4)
+    (mutated, score) = probable_mutations(original_string, pmf, flexibility_dict, 4, method='min')
+    print mutated
 
-    
 
 if __name__ == '__main__':
     test()
